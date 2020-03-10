84093b1408bfff5222387d4ab4a2f61d18a9d58f
Sabrina WANG <sabrina.wang.siyu@gmail.com>
2020-03-10 19:27:13 +0100

    modified launcher to run frontend and backend at the same time

--- launcher.sh
+++ launcher.sh
@@ -1,1 +1,6 @@
-gunicorn -w 4 -b 127.0.0.1:8000 api_run:app
+# Run the backend on background enviroment and run the frontend at the front
+# To see the gunicorn process running on background : ps ax|grep gunicorn
+# To stop the gunicorn process : kill <reference number of process> 
+#                                (one kill for front end + one kill for backend)
+gunicorn -w 4 -b 127.0.0.1:5000 api_front:app &
+gunicorn -w 4 -b 127.0.0.1:8000 api_run:app &
