119e8ea28aeb508c09a941936f7a2e80eb55b626
Pierre Rochet <pierre-rochet@outlook.fr>
2020-03-10 15:05:29 +0100

    form add data

--- api_front.py
+++ api_front.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 api = Api(app)
 
 api_backend = "https://api.ceptyconsultant.localhost"
-current_user = {}
+
 
 def _check_cookie_auth():
 	token = request.cookies.get('token')
-	print("TTTT",token)
 	return token
 
 class Login(Resource):
 	"""Login"""
+
 	def get(self):
 		render_login = render_template("login.html")
 		resp = Response(render_login, status=200, content_type="text/html")
@@ -30,6 +30,8 @@ class Login(Resource):
 		r = requests.post(api_backend + '/login', json=user_id, verify=False)
 		
 		token = r.json()["Token"]
+
+		global current_user
 		current_user = r.json()["User"]
 		
 		resp = redirect('/data')
@@ -38,6 +40,7 @@ class Login(Resource):
 		return resp
 
 class Data(Resource):
+
 	def get(self, contrib_name=None, public_id=None):
 		auth = _check_cookie_auth()
 		if auth == None:
@@ -53,12 +56,53 @@ class Data(Resource):
 		if public_id!=None and contrib_name!=None:
 			r = requests.get(api_backend + "/data/" + contrib_name + "/" + public_id, headers=headers, verify=False)
 		
-		render_data = render_template("data.html")
+		# global current_user
+		# print(current_user)
+		# params={
+		# 	"username":current_user["username"]
+		# }
+		
+		data = r.json()["data"]
+		render_data = render_template(
+			"data.html",
+			username="Mikolov",
+			data=data
+		)
 		resp = Response(render_data, status=200, content_type="text/html")
 
 		# resp = r.json()
 		return resp
 		
+	def post(self):
+
+		auth = _check_cookie_auth()
+		if auth == None:
+			abort(403)
+		headers = { 'x-access-tokens' : auth }
+
+
+		data_add = request.form
+		data_set = {
+			"article_id":data_add["article_id"],
+			"contrib_data":data_add["contrib_data"],
+			"contrib_name":data_add["contrib_name"],
+			"contrib_path":data_add["contrib_path"],
+			"contrib_type":data_add["contrib_type"],
+			"dico_id":data_add["dico_id"],
+			"last_update":data_add["last_update"],
+			"ntealan":data_add["ntealan"],
+			"public_id":data_add["public_id"],
+			"user_id":data_add["user_id"],
+			"user_name":data_add["user_name"],
+			"validate":data_add["validate"]
+		}
+
+		r = requests.put(api_backend + '/data', json=data_set, headers=headers, verify=False)
+		
+		resp = redirect('/data')
+		return resp
+
+
 
 
 api.add_resource(Login, "/", "/login")
