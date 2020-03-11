3c7b11db76147e27422aef136e8040db126338b0
Pierre Rochet <pierre-rochet@outlook.fr>
2020-03-11 11:29:21 +0100

    fix:http->https add:search

--- api_front.py
+++ api_front.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask, request, json, jsonify, request, Response, make_response, abort, redirect, render_template, after_this_request
+from flask import Flask, request, json, jsonify, request, Response, make_response, abort, redirect, render_template, url_for
 from flask_restful import Resource, Api
 import json
 import requests
@@ -8,6 +8,7 @@ app = Flask(__name__)
 api = Api(app)
 
 api_backend = "https://api.ceptyconsultant.localhost"
+
 
 
 def _check_cookie_auth():
@@ -20,6 +21,9 @@ class Login(Resource):
 	def get(self):
 		render_login = render_template("login.html")
 		resp = Response(render_login, status=200, content_type="text/html")
+		auth = _check_cookie_auth()
+		if auth != None:
+			resp.delete_cookie('token', domain="ceptyconsultant.localhost")
 		return resp
 
 	def post(self):
@@ -31,8 +35,9 @@ class Login(Resource):
 		
 		token = r.json()["Token"]
 
-		global current_user
-		current_user = r.json()["User"]
+		#global current_user
+		# current_user = r.json()["User"]
+		current_user="Mikolov"
 		
 		resp = redirect('/data')
 		resp.set_cookie("token", token, domain="ceptyconsultant.localhost")
@@ -58,56 +63,110 @@ class Data(Resource):
 		
 		# global current_user
 		# print(current_user)
-		# params={
-		# 	"username":current_user["username"]
-		# }
-		
-		data = r.json()["data"]
-		render_data = render_template(
-			"data.html",
-			username="Mikolov",
-			data=data
-		)
-		resp = Response(render_data, status=200, content_type="text/html")
 
-		# resp = r.json()
-		return resp
+		#username = current_user["username"]
+		username = "Mikolov"
+		
+		data_get = r.json()
+		if "data" in data_get:
+			data = data_get["data"]
+			render_data = render_template(
+				"data.html",
+				current_user=username,
+				data=data
+			)
+			resp = Response(render_data, status=200, content_type="text/html")
+			return resp
+		
+		else :
+			abort(404)
+		
 		
 	def post(self):
-
+		print("post")
 		auth = _check_cookie_auth()
 		if auth == None:
 			abort(403)
-		headers = { 'x-access-tokens' : auth }
-
-
-		data_add = request.form
-		data_set = {
-			"article_id":data_add["article_id"],
-			"contrib_data":data_add["contrib_data"],
-			"contrib_name":data_add["contrib_name"],
-			"contrib_path":data_add["contrib_path"],
-			"contrib_type":data_add["contrib_type"],
-			"dico_id":data_add["dico_id"],
-			"last_update":data_add["last_update"],
-			"ntealan":data_add["ntealan"],
-			"public_id":data_add["public_id"],
-			"user_id":data_add["user_id"],
-			"user_name":data_add["user_name"],
-			"validate":data_add["validate"]
-		}
-
-		r = requests.put(api_backend + '/data', json=data_set, headers=headers, verify=False)
 		
-		resp = redirect('/data')
+		data_form = request.form
+
+		if data_form['action'] == 'delete':
+			article_id = data_form["article_id"]
+			headers = { 'x-access-tokens' : auth }
+			r = requests.delete(api_backend + '/data?article_id=' + article_id, headers=headers, verify=False)
+			resp = redirect('/data')
+			return resp
+		
+		elif data_form['action'] == 'edit':
+			article_id = data_form["article_id"]
+			data_set_1 = {
+				"contrib_data":data_form["contrib_data"],
+				"contrib_name":data_form["contrib_name"],
+				"contrib_path":data_form["contrib_path"],
+				"contrib_type":data_form["contrib_type"],
+				"dico_id":data_form["dico_id"],
+				"last_update":data_form["last_update"],
+				"ntealan":data_form["ntealan"],
+				"public_id":data_form["public_id"],
+				"user_id":data_form["user_id"],
+				"user_name":data_form["user_name"],
+				"validate":data_form["validate"]
+			}
+
+			headers = { 'x-access-tokens' : auth }
+			r = requests.post(api_backend + '/data?article_id=' + article_id, json=data_set_1, headers=headers, verify=False)
+			
+			
+			print("TESTTTTTTTT", r.json())
+			resp = redirect('/data')
+			return resp
+
+		elif data_form['action'] == 'add':
+			
+			data_set = {
+				"article_id":data_form["article_id"],
+				"contrib_data":data_form["contrib_data"],
+				"contrib_name":data_form["contrib_name"],
+				"contrib_path":data_form["contrib_path"],
+				"contrib_type":data_form["contrib_type"],
+				"dico_id":data_form["dico_id"],
+				"last_update":data_form["last_update"],
+				"ntealan":data_form["ntealan"],
+				"public_id":data_form["public_id"],
+				"user_id":data_form["user_id"],
+				"user_name":data_form["user_name"],
+				"validate":data_form["validate"]
+			}
+			headers = { 'x-access-tokens' : auth }
+			r = requests.put(api_backend + '/data', json=data_set, headers=headers, verify=False)
+			resp = redirect('/data')
+			return resp
+		
+		elif data_form['action'] == 'search':
+			headers = { 'x-access-tokens' : auth }
+			contrib_name  = data_form['contrib_name']
+			if data_form['public_id'] == '' : public_id = None
+			else : public_id  = data_form['public_id']
+			print(contrib_name, public_id)
+			resp = redirect(url_for('data', contrib_name=contrib_name, public_id=public_id))
+			return resp
+
+
+class Licence(Resource):
+	def get(self):
+		auth = _check_cookie_auth()
+		if auth == None:
+			abort(403)
+
+		headers = { 'x-access-tokens' : auth }
+		render_licence = render_template("licence.html")
+		resp = Response(render_licence, status=200, content_type="text/html")
 		return resp
 
 
-
-
 api.add_resource(Login, "/", "/login")
-api.add_resource(Data, "/data",
-"/data/<string:contrib_name>",
+api.add_resource(Licence, "/licence")
+api.add_resource(Data, "/data", "/data/<string:contrib_name>",
 "/data/<string:contrib_name>/<string:public_id>")
 
 
