500af8107d3e513a281ccde36dc95891e7c12153
Pierre Rochet <pierre-rochet@outlook.fr>
2020-03-13 22:03:57 +0100

    update test

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import requests
 
 url = 'https://api.ceptyconsultant.localhost'
 
-
 # SANS LOGIN =======================================
 
 # GET
@@ -44,7 +43,8 @@ assert r.json() == {'message': 'token manquant'}
 
 # OBTENIR LE TOKEN ================================
 # login et obtenir le token
-r = requests.post(url + '/login', json={"username":"Thomas", "password":"Mikolov"}, verify=False)
+r = requests.post(url + '/login', json={"username":"Mikolov", "password":"4zr5HE"}, verify=False)
+#print(r.json())
 assert "Token" in r.json()
 token = r.json()["Token"]
 headers = { 'x-access-tokens' : token }
@@ -59,7 +59,7 @@ assert 'data' in r.json()
 
 # DELETE
 # supprimer des données
-params = {'article_id': '93ecdcd3-c570-4891-94ac-e4c7d449a7bc'}
+params = {'article_id': 'b9512669-8144-43ed-81d8-a192f92c4cd3'}
 headers = { 'x-access-tokens' : token }
 r = requests.delete(url + '/data', headers=headers, params=params, verify=False)
 #print(r.json())
@@ -69,7 +69,7 @@ assert r.json() == {'OK': 'Article supprimé avec succés'}
 # PUT
 # ajouter des données
 contrib = {
-    "article_id": "93ecdcd3-c570-4891-94ac-e4c7d449a7bc",
+    "article_id": "b9512669-8144-43ed-81d8-a192f92c4cd3",
     "contrib_data": "mlem-_sound_2-2019-09-03T193015.084Z-.wav",
     "contrib_name": "m\u025b\u0301le\u0304m",
     "contrib_path": "https://ntealan.net/soundcontrib/",
@@ -88,4 +88,29 @@ r = requests.put(url + '/data', headers=headers, json=contrib, verify=False )
 #print(r.json())
 assert r.json() == {'OK': 'Article ajouté avec succès'}
 
+
+# POST
+# modifier des données
+contrib = {
+    "article_id": "b9512669-8144-43ed-81d8-a192f92c4cd3",
+    "contrib_data": "mlem-_sound_2-2019-09-03T193015.084Z-.wav",
+    "contrib_name": "m\u025b\u0301le\u0304m",
+    "contrib_path": "https://ntealan.net/soundcontrib/",
+    "contrib_type": "sound",
+    "dico_id": "yb_fr_3031",
+    "last_update": "2019-09-03 19:32:51.447000",
+    "ntealan": False,
+    "public_id": "5e328321-e943-4ab5-b892-3d45ed4f94dd",
+    "user_id": "b42e96a8-7b0b-8b45-ae69-7c2efd472e1d",
+    "user_name": "Bergier",
+    "validate": False
+}
+
+params = {'article_id': 'b9512669-8144-43ed-81d8-a192f92c4cd3'}
+headers = { 'x-access-tokens' : token }
+r = requests.post(url + '/data', headers=headers, json=contrib, params=params, verify=False )
+print(r.json())
+assert r.json() == {'OK': 'Données modifées avec succès'}
+
+print("==================")
 print("--> test success !")
